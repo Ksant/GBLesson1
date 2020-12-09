d2e95851d7acae587dc58a3fb68237f11ee7179e
Ksant <mkonstn@gmail.com>
2020-12-09 12:50:53 +0300

    Инициализация

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите пожалуйста свое имя.");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToLongDateString()}");
+            Console.ReadLine();
+        }
+    }
+}
